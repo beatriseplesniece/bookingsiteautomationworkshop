4e45742317bc383f40c4892fce4eb66d8bbd0259
beatriseplesniece <beatrise.plesniece@testdevlab.com>
2018-07-25 10:00:16 +0300

    lecture3 update

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -3,16 +3,16 @@ package stepdefinitions;
 import cucumber.api.java.After;
 import cucumber.api.java.Before;
 
-import static com.codeborne.selenide.Selenide.close;
-import static com.codeborne.selenide.Selenide.open;
+
+import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.CHROME;
+
 
 public class Hooks {
 
     @Before
     public void launchBrowser() {
         System.setProperty("selenide.browser", CHROME);
-        ;
     }
 
     @Before
